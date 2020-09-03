bcd9fd4787ccf4c9f6ebc0bf33c0e3eb29c41750
Priya-unique <68942167+Priya-unique@users.noreply.github.com>
2020-09-04 00:24:15 +0530

    Update scripts.js

--- module-4/scripts.js
+++ module-4/scripts.js
@@ -1,8 +1,8 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i=0;i<names.length;i++) {
   if (names[i][0]=='j'||names[i][0]=='J') {
-    console.log("Good Bye " + names[i]);
+    byespeak(names[i]);
   } else {
-    console.log("Hello " + names[i]);
+    hellospeak(names[i]);
   }
 }
