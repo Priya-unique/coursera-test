ffe93af5c256f5b044e01206f429c4adfb9884ca
Priya-unique <68942167+Priya-unique@users.noreply.github.com>
2020-09-04 00:52:10 +0530

    Update scripts.js

--- module-4/scripts.js
+++ module-4/scripts.js
@@ -1,10 +1,8 @@
-(function(window){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i=0;i<names.length;i++) {
+for (var i=0;i < names.length;i++) {
   if (names[i][0]=='j'||names[i][0]=='J') {
     byespeak(names[i]);
   } else {
     hellospeak(names[i]);
   }
- }
-})(window);
+}
