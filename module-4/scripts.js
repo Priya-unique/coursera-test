161a6c37ee0746ad8b7545ada39c2604776a823a
Priya-unique <68942167+Priya-unique@users.noreply.github.com>
2020-09-04 12:18:41 +0530

    Update scripts.js

--- module-4/scripts.js
+++ module-4/scripts.js
@@ -1,9 +1,10 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i=0;i<names.length;i++) {
+  var speakWord="";
   if (names[i][0]=='j'||names[i][0]=='J') {
-      var speakWord = "Good Bye";
+      speakWord = "Good Bye";
   } else {
-      var speakWord = "Hello";
+      speakWord = "Hello";
   }
   document.write(speakWord + " " + name[i]);
 }
