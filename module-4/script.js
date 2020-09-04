8c10e0a6e4cc28ce435f4669c2c6e68b0526c55f
Priya-unique <68942167+Priya-unique@users.noreply.github.com>
2020-09-04 14:36:55 +0530

    Update script.js

--- module-4/script.js
+++ module-4/script.js
@@ -31,7 +31,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
+(function(window){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -39,7 +39,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for (var i=0;i<names.length;i++) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -55,9 +55,10 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  } else {
-    // helloSpeaker.xxxx
-  }
-}
+   if (names[i][0]=='j'||names[i][0]=='J') {
+     window.byeSpeaker.speak(names[i]);
+   } else {
+     window.helloSpeaker.soeak(names[i]);
+   }
+ }
+})(window);
